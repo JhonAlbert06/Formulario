4c603b0c050d1da33d0ac4f3cc1faa24cf9a0ad7
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-01-15 19:49:27 -0300

    Terminado

--- Ventana.java
+++ Ventana.java
@@ -16,8 +16,6 @@ public class Ventana extends JFrame{
 	private JButton btNuevo, btGuardar, btEliminar, busqueda;
 
 	private JPanel panelPrincipal;
-    private String archivoNombre;
-    private JMenuBar nivel;
 	private JCheckBox activo;
 	private JTextField id, alias, nombres, email, costoxHora;
 	private JPasswordField clave, confirmacionClave;
@@ -27,11 +25,6 @@ public class Ventana extends JFrame{
     }
 
     private void init(){
-
-		/*
-		JPanel p = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS)); 
-		*/
         
 		panelPrincipal = new JPanel();
 		panelPrincipal.setLayout(null);
@@ -41,53 +34,73 @@ public class Ventana extends JFrame{
 		id_.setBounds(10, 15, 20, 35);
 		panelPrincipal.add(id_);
 		id = new JTextField(10);
-		id.setBounds(25, 25, 100, 20);
+		id.setBounds(69, 25, 100, 20);
 		panelPrincipal.add(id);
 
-		busqueda = new JButton("Busqueda");
-		busqueda.setBounds(130, 25, 50, 20);
+		ImageIcon icon = new ImageIcon("C:\\Users\\Jhony Alberto\\Documents\\Code\\Formulario\\icons8-search-24.png");
+		busqueda = new JButton(icon);
+		busqueda.setBounds(180, 25, 50, 20);
 		panelPrincipal.add(busqueda);
 
 		activo = new JCheckBox("Activo");
 		activo.setBounds(400, 25, 100, 20);
 		panelPrincipal.add(activo);
 
+		JLabel nivel_ = new JLabel("Nivel");
+		nivel_.setBounds(380, 60, 100, 25);
+		panelPrincipal.add(nivel_);
+		JMenu c = new JMenu("Administrador");
+		
+		c.setBounds(420, 60, 100, 25);
+		panelPrincipal.add(c);
+
 		JLabel alias_ = new JLabel("Alias ");
 		alias_.setBounds(10, 60, 50, 35);
 		panelPrincipal.add(alias_);
 		alias = new JTextField(10);
-		alias.setBounds(70, 60, 100, 20);
+		alias.setBounds(70, 70, 150, 20);
 		panelPrincipal.add(alias);
 
-		JLabel nombres_ = new JLabel("Nombres ");
+		JLabel nombres_ = new JLabel("Nombres");
+		nombres_.setBounds(10, 110, 100, 20);
 		panelPrincipal.add(nombres_);
 		nombres = new JTextField(10);
+		nombres.setBounds(70, 110, 200, 20);
 		panelPrincipal.add(nombres);
 
 		JLabel email_ = new JLabel("Email ");
+		email_.setBounds(300, 110, 100, 20);
 		panelPrincipal.add(email_);
 		email = new JTextField(10);
+		email.setBounds(350, 110, 200, 20);
 		panelPrincipal.add(email);
 		
 		JLabel clave_ = new JLabel("Clave ");
+		clave_.setBounds(10, 160, 100, 20);
 		panelPrincipal.add(clave_);
 		clave = new JPasswordField(16);
+		clave.setBounds(70, 160, 90, 20);
 		panelPrincipal.add(clave);
 
 		JLabel confirmacionClave_ = new JLabel("Confirma Clave ");
+		confirmacionClave_.setBounds(170, 160, 100, 20);
 		panelPrincipal.add(confirmacionClave_);
 		confirmacionClave = new JPasswordField(16);
+		confirmacionClave.setBounds(265, 160, 100, 20);
 		panelPrincipal.add(confirmacionClave);
 
 		JLabel costoXhora_ = new JLabel("Costo X Hora ");
+		costoXhora_.setBounds(370, 160, 100, 20);
 		panelPrincipal.add(costoXhora_);
 		costoxHora = new JTextField(" ", 10);
+		costoxHora.setBounds(450, 160, 100, 20);
 		panelPrincipal.add(costoxHora);
 
 		panelBotones = new JPanel();
 		add(panelBotones, BorderLayout.SOUTH);
 
-		btNuevo = new JButton("Nuevo");
+		ImageIcon icon1 = new ImageIcon("C:\\Users\\Jhony Alberto\\Documents\\Code\\Formulario\\icons8-new-24.png");
+		btNuevo = new JButton("Nuevo", icon1);
 		btNuevo.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
@@ -97,7 +110,8 @@ public class Ventana extends JFrame{
 		});
 		panelBotones.add(btNuevo);
 
-		btGuardar = new JButton("Guardar");
+		ImageIcon icon2 = new ImageIcon("C:\\Users\\Jhony Alberto\\Documents\\Code\\Formulario\\icons8-save-24.png");
+		btGuardar = new JButton("Guardar", icon2);
 		btGuardar.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
@@ -106,7 +120,8 @@ public class Ventana extends JFrame{
 		});
 		panelBotones.add(btGuardar);
 
-		btEliminar = new JButton("Eliminar");
+		ImageIcon icon3 = new ImageIcon("C:\\Users\\Jhony Alberto\\Documents\\Code\\Formulario\\icons8-delete-24.png");
+		btEliminar = new JButton("Eliminar", icon3);
 		btEliminar.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
